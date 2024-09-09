e70799d834040c028ae4323bcedfa6739624456d
Sainesh Nakra <30744887+saineshnakra@users.noreply.github.com>
2024-09-08 20:53:47 -0400

    fix

--- runcheck/process_errors.py
+++ runcheck/process_errors.py
@@ -176,7 +176,7 @@ def check_and_update_requirements(file_path):
     print("The output has been saved to updated_requirement_from_gpt.txt")
 
 # Read the build_check_results.csv file
-df = pd.read_csv("runcheck/build_check_results.csv")
+df = pd.read_csv("build_check_results.csv")
 
 # Filter the repositories that do not have "success" status
 error_repos = df[~df['status'].str.contains("success", case=False)]['file_or_repo'].tolist()
